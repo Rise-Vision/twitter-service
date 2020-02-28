0deccae2af4d13784b0f6918e52c3198326ea85b
santiagonoguez <santiago@nul-unu.com>
2020-02-28 14:18:39 -0600

    rename function

--- src/redis-cache/api.js
+++ src/redis-cache/api.js
@@ -2,11 +2,11 @@ const redis = require("redis-promise");
 
 const statusKeyFor = username => `${ username }:status`;
 
-const asJSON = value => value ? JSON.parse(value) : null;
+const parseJSON = value => value ? JSON.parse(value) : null;
 
 const getStatusFor = username => {
   return redis.getString(statusKeyFor(username))
-  .then(asJSON);
+  .then(parseJSON);
 };
 
 const saveStatus = (username, status) => {
