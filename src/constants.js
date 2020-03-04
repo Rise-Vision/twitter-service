a0bb4ce987a4082203478396cab4d3143fc02dd1
Stuart Lees <stuartlees@gmail.com>
2020-03-04 16:39:46 -0500

    Format images value for get-tweets response

--- src/constants.js
+++ src/constants.js
@@ -3,5 +3,7 @@ module.exports = {
   FORBIDDEN_ERROR: 403,
   CONFLICT_ERROR: 409,
   CONFLICT_ERROR_MESSAGE: "Another request is already loading user timeline. Please retry in a few seconds.",
-  SERVER_ERROR: 500
+  SERVER_ERROR: 500,
+  MEDIA_TYPE_IMAGE: "photo",
+  MEDIA_TYPE_ANIMATED_GIF: "animated_gif"
 };
--- src/timelines/data_formatter.js
+++ src/timelines/data_formatter.js
@@ -1,5 +1,38 @@
 /* eslint-disable no-warning-comments */
 
+const constants = require('../constants');
+const allowedMediaTypes = [constants.MEDIA_TYPE_IMAGE, constants.MEDIA_TYPE_ANIMATED_GIF];
+
+const isExpectedMediaFormat = (tweet) => {
+  if (!("extended_entities" in tweet)) {return false}
+
+  if (!("media" in tweet.extended_entities)) {return false}
+
+  if (!Array.isArray(tweet.extended_entities.media) || tweet.extended_entities.media.length === 0) {return false;}
+
+  if (!("type" in tweet.extended_entities.media[0])) {return false}
+
+  return allowedMediaTypes.includes(tweet.extended_entities.media[0].type);
+};
+
+const getFormattedImageURL = (imageItem) => {
+  if (!("media_url_https" in imageItem)) {return ""}
+
+  const baseUrl = imageItem.media_url_https.substring(0, imageItem.media_url_https.lastIndexOf(".")),
+    extension = imageItem.media_url_https.slice(imageItem.media_url_https.lastIndexOf(".") + 1),
+    name = "large";
+
+  return `${baseUrl}?format=${extension}&name=${name}`
+};
+
+const getImagesField = (tweet) => {
+  if (!isExpectedMediaFormat(tweet)) {return []}
+
+  const images = tweet.extended_entities.media.map(imageItem => getFormattedImageURL(JSON.parse(JSON.stringify(imageItem))));
+
+  return images.filter(url => url !== "");
+};
+
 const getStatisticsFields = (tweet) => {
   return {
     retweetCount: "retweet_count" in tweet ? tweet.retweet_count : null,
@@ -46,7 +79,8 @@ const getRootFields = (tweet) => {
 
   return Object.assign({}, userFields, {
     createdAt: "created_at" in tweet ? tweet.created_at : null,
-    text: getTextField(tweet)
+    text: getTextField(tweet),
+    images: getImagesField(tweet)
   })
 };
 
