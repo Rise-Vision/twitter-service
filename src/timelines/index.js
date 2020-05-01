7625da64538cc377c38e66f0239629839bbd83ea
Stuart Lees <stuartlees@gmail.com>
2020-05-01 11:41:21 -0400

    Implement providing mock data based on 'demo' presentation id

--- src/timelines/index.js
+++ src/timelines/index.js
@@ -9,6 +9,7 @@ const core = require('../core');
 const {currentTimestamp} = require('../utils');
 const formatter = require('./data_formatter');
 const utils = require('../utils');
+const mockData = require('./mock-data');
 
 const {
   BAD_REQUEST_ERROR, CONFLICT_ERROR, CONFLICT_ERROR_MESSAGE, FORBIDDEN_ERROR,
@@ -278,6 +279,16 @@ const handleGetTweetsRequest = (req, res) => {
   .catch(error => logAndSendError(res, error, BAD_REQUEST_ERROR));
 }
 
+const handleDemoTweetsRequest = (res) => {
+  const tweets = mockData.tweets().slice();
+
+  res.header("Cache-control", `private, max-age=0`);
+
+  res.json({
+    tweets
+  });
+}
+
 const validatePresentationQueryParams = (req) => {
   const {presentationId, componentId, hash, useDraft} = req.query;
 
@@ -346,11 +357,20 @@ const handleGetTweetsEncryptedRequest = (req, res) => {
     .then(params => {
       const {presentationId, componentId, username} = params;
 
+      // for rise-data-twitter e2e purposes and creative local development purposes
+      if (presentationId === "demo") {
+        return {companyId: "demo"}
+      }
+
       // TODO: decrypt username
 
       return core.getPresentationWithoutHash(presentationId, componentId, username);
     })
     .then(presentation => {
+      if (presentation.companyId === "demo") {
+        return handleDemoTweetsRequest(res);
+      }
+
       req.query = {...req.query, ...presentation};
 
       return handleGetTweetsRequest(req, res);
@@ -367,6 +387,7 @@ const handleGetTweetsEncryptedRequest = (req, res) => {
 }
 
 module.exports = {
+  handleDemoTweetsRequest,
   handleGetTweetsRequest,
   handleGetTweetsEncryptedRequest,
   handleGetPresentationTweetsRequest
--- test/unit/timelines.tests.js
+++ test/unit/timelines.tests.js
@@ -11,9 +11,11 @@ const timelines = require("../../src/timelines");
 const formatter = require("../../src/timelines/data_formatter");
 const twitter = require("../../src/twitter");
 const utils = require("../../src/utils");
+const mockData = require("../../src/timelines/mock-data");
 
 const sample30Tweets = require("./samples/tweets-30").data;
 const sampleTweets = require("./samples/tweets-timeline").data;
+const mockDataTweets = mockData.tweets();
 
 const sampleTweetsFormatted = formatter.getTimelineFormatted(sampleTweets);
 
@@ -52,6 +54,32 @@ describe("Timelines", () => {
 
   afterEach(() => {
     simple.restore();
+  });
+
+  describe("handleDemoTweetsRequest", () => {
+    it("should return mock data", () => {
+      timelines.handleDemoTweetsRequest(res);
+
+      assert(res.json.called);
+      assert.deepEqual(res.json.lastCall.args[0], {
+        tweets: mockDataTweets
+      });
+
+      assert.equal(res.header.callCount, 1);
+      assert(!res.status.called);
+      assert(!res.send.called);
+
+      assert.equal(res.header.lastCall.args[0], "Cache-control");
+
+      const header = res.header.lastCall.args[1];
+      assert(header);
+
+      const fragments = header.split("=");
+      assert.equal(fragments[0], "private, max-age");
+
+      const expiration = Number(fragments[1]);
+      assert.equal(expiration, 0);
+    });
   });
 
   describe("handleGetTweetsRequest / validation", () => {
@@ -653,4 +681,5 @@ describe("Timelines", () => {
         });
     });
   });
+
 });
